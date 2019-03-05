af40e2c5deae2c8f116cda99c71b5c109835da35
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2019-03-04 19:36:21 -0500

    welcome function with random names

--- green-dnd.py
+++ green-dnd.py
@@ -1,7 +1,38 @@
+import names
+
+def get_player_name():
+    # LOCAL VARIABLES
+    # The player enters their name and gets assigned to a variable called "name"
+
+
+    # This is just an alternative name that the game wants to call the player
+    alt_name = names.get_full_name()
+    answer = input("We have a name for you. Your name is {}, Do you want to play this name ? [Y|N] > ".format(alt_name.upper()))
+    name = "vignesh"
+    if answer.lower() in ["y", "yes"]:
+        name = alt_name
+        print("You are fun, {}! Let's begin our adventure!".format(name.upper()))
+    elif answer.lower() in ["n", "no"]:
+        name = input("What's your name? > ")
+        print("Ok, picky. {} it is. Let's get started on our adventure.".format(name.upper()))
+    else:
+        print("Trying to be funny? Well, you will now be called {} anyway.".format(alt_name.upper()))
+        name = alt_name
+    print("----")
+    # Now notice that we are returning the variable called name.
+    # In main(), it doesn't know what the variable "name" is, as it only exists in
+    # get_player_name() function.
+    # This is why indentation is important, variables declared in this block only exists in that block
+    return name
+
+
+
 def main():
     '''
     Gets the players name by calling get_player_name() before starting the adventure.
     '''
+    print("\n Welcome to Green CASTLE")
+    player_name = get_player_name()
 
     ####################################################################
     # ACTIVITIES
@@ -25,9 +56,8 @@ def main():
     # object-orient programming, which is another lesson for another day.
     #
     #####################################################################
-    print("\n Welcome to Green CASTLE")
     print("\nThe end\n")
-    print(f"Thanks for playing")
+    print("Thanks for playing, {}".format(player_name.upper()))
 
 
 if __name__ == '__main__':
